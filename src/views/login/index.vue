82792c751f14164cbd71e1210b0b5c37526d8c21
YuanS1119 <1650216381@qq.com>
2019-08-19 14:19:13 +0800

    登录页面背景加入

---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="login"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.login{
+width: 100%;
+height: 100vh;
+background-image: url('../../img/login_bg.jpg');
+}
+</style>
