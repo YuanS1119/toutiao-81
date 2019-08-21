02620aca9df39e814ce49b9c86bf5a5e206ed320
YuanS1119 <1650216381@qq.com>
2019-08-21 19:49:36 +0800

    统一全局注册插件的使用方式Vue.use

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -16,13 +16,7 @@
 </template>
 
 <script>
-import layoutAside from '../../components/home/layout-aside'
-import layoutHeader from '../../components/home/layout-header'
 export default {
-  components: {
-    'el-aside': layoutAside,
-    'el-header': layoutHeader
-  }
 }
 </script>
 <style lang="less" scoped>
