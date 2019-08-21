fed2e8e89120d14d2a42736ed9df26199766e501
YuanS1119 <1650216381@qq.com>
2019-08-21 19:33:25 +0800

    home页面布局及完成及其默认首页main完成(二次提交)

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,50 @@
+<template>
+    <el-row class="header_row" type="flex" justify="space-between">
+        <el-col :span="10" class="header_left">
+            <i class="el-icon-s-unfold" style="font-size:20px"></i>
+            <span>江苏传智播客教育科技股份有限公司</span>
+        </el-col>
+        <el-col :span="3" class="header_right">
+            <img src="../../assets/img/avatar.jpg" alt="" class="img">
+            <el-dropdown trigger="click">
+                <span class="el-dropdown-link">
+                    昵称
+                    <i class="el-icon-arrow-down el-icon--right"></i>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item >个人信息</el-dropdown-item>
+                    <el-dropdown-item >git地址</el-dropdown-item>
+                    <el-dropdown-item >退出</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+    .header_row{
+        padding: 15px 0;
+        .header_left{
+            padding-left: 10px;
+            font-size: 15px;
+            display: flex;
+            align-items :center;
+        }
+        .header_right{
+            display: flex;
+            align-items: center;
+            .img{
+                width: 40px;
+                height: 40px;
+                border-radius: 50%;
+                margin-right: 10px;
+            }
+        }
+    }
+</style>
---
+++ src/views/home/main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home_main">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+    .home_main{
+        background-image: url(../../assets/img/home.jpg);
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+    }
+</style>
