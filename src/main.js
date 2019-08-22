689b449810e4bac8c1562b5810e3ec25c7b49afe
YuanS1119 <1650216381@qq.com>
2019-08-22 08:50:35 +0800

    主页模块-导航守卫

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+
 import ElementUI from 'element-ui' // 引入UI
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styles/index.less'
 import components from './components'
 import axios from 'axios'
+import router from './permission'
 
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios
---
+++ src/permission.js
@@ -0,0 +1,19 @@
+import router from './router'
+router.beforeEach(function (to, from, next) {
+  if (to.path.startsWith('/home')) {
+    let result = window.localStorage.getItem('userInfo')
+    if (result) {
+      let userinfo = JSON.parse(result)
+      if (userinfo && userinfo.token) {
+        next()
+      } else {
+        next('/login')
+      }
+    } else {
+      next('/login')
+    }
+  } else {
+    next()
+  }
+})
+export default router
