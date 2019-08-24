5e72322049b5fe94eccc462378b86f75f1705ac0
YuanS1119 <1650216381@qq.com>
2019-08-24 23:54:57 +0800

    页面结构-分页请求

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,48 +1,46 @@
 <template>
   <el-card class="out">
-      <bread-crumb slot="header">
-        <span slot="title">内容列表</span>
-      </bread-crumb>
-      <el-form>
-        <el-form-item label="文章状态：">
-          <el-radio-group v-model="searchParams.status" @change="refreshList">
-            <el-radio :label="5">全部</el-radio>
-            <el-radio :label="0">草稿</el-radio>
-            <el-radio :label="1">待审核</el-radio>
-            <el-radio :label="2">审核通过</el-radio>
-            <el-radio :label="3">审核失败</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="频道列表：">
-          <el-select v-model="searchParams.channels_id" placeholder="请选择">
-            <el-option
-              v-for="item in channels"
-              :key="item.id"
-              :label="item.label"
-              :value="item.name"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="时间选择：">
-          <el-date-picker
-            v-model="searchParams.dataRange"
-            @change="refreshList"
-            value-format="yyyy-MM-dd"
-            type="daterange"
-            range-separator="-"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期"
-          ></el-date-picker>
-        </el-form-item>
-      </el-form>
+    <bread-crumb slot="header">
+      <span slot="title">内容列表</span>
+    </bread-crumb>
+    <el-form>
+      <el-form-item label="文章状态：">
+        <el-radio-group v-model="searchParams.status" @change="refreshList">
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="频道列表：">
+        <el-select v-model="searchParams.channels_id" placeholder="请选择">
+          <el-option v-for="item in channels" :key="item.id" :label="item.label" :value="item.name"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="时间选择：">
+        <el-date-picker
+          v-model="searchParams.dataRange"
+          @change="refreshList"
+          value-format="yyyy-MM-dd"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+    </el-form>
     <div class="conatiner">
       <div class="title">共找到{{page.total}}条符合条件的内容</div>
-      <div class="list"  v-for="(item,id) in content" :key="id">
+      <div class="list" v-for="(item,id) in content" :key="id">
         <div class="left">
-          <img src="../../assets/img/404.png" alt />
+          <img :src="item.cover.images[0]" alt />
           <div class="content">
             <span>{{item.title}}</span>
-            <el-tag :type="item.status | setStatusStyle" style="width:60px;">{{item.status | setStatus}}</el-tag>
+            <el-tag
+              :type="item.status | setStatusStyle"
+              style="width:60px;"
+            >{{item.status | setStatus}}</el-tag>
             <span>{{item.pubdate}}</span>
           </div>
         </div>
@@ -58,6 +56,17 @@
         </div>
       </div>
     </div>
+
+    <!-- 分页组件 -->
+    <el-row type="flex" justify="center" style="margin:20px 0;">
+      <el-pagination
+        layout="prev, pager, next"
+        :total="page.total"
+        :page-size="page.pageSize"
+        :current-page="page.currentPage"
+        @current-change="currentChange"
+      ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -68,7 +77,9 @@ export default {
       channels: [],
       content: {}, // 内容列表
       page: {
-        total: 0
+        total: 0,
+        currentPage: 1,
+        pageSize: 10
       }, // 页数
       searchParams: {
         status: 5,
@@ -78,6 +89,27 @@ export default {
     }
   },
   methods: {
+    getParams () {
+      let { status, channels_id: cid, dataRange } = this.searchParams
+      let params = {
+        status: status === 5 ? null : status,
+        channels_id: cid,
+        begin_pubdate: dataRange && dataRange.length ? dataRange[0] : null,
+        end_pubdate: dataRange && dataRange.length > 1 ? dataRange[1] : null
+      }
+      params.page = this.page.currentPage
+      params.per_page = this.page.pageSize
+      return params
+    },
+    currentChange (newPage) {
+      this.page.currenrPage = newPage
+      this.getArticles(this.getParams())
+    },
+    // 刷新列表数据 状态改变/频道切换/日期改变 都会触发
+    refreshList () {
+      this.page.currentPage = 1
+      this.getArticles(this.getParams())
+    },
     getArticles (params) {
       this.$axios({
         url: '/articles',
@@ -93,16 +125,6 @@ export default {
       }).then(res => {
         this.channels = res.data.channels
       })
-    },
-    refreshList () {
-      let { status, channels_id: cid, dataRange } = this.searchParams
-      let params = {
-        status: status === 5 ? null : status,
-        channels_id: cid,
-        begin_pubdate: dataRange && dataRange.length ? dataRange[0] : null,
-        end_pubdate: dataRange && dataRange.length > 1 ? dataRange[1] : null
-      }
-      this.getArticles(params)
     }
   },
   filters: {
@@ -130,40 +152,38 @@ export default {
           return 'success'
         case 3:
           return 'info'
-        case 4:
-          return ''
       }
     }
   },
   created () {
-    this.getArticles()
+    this.getArticles({ page: 1, per_page: 10 })
     this.getChannels()
   }
 }
 </script>
 
 <style lang='less' scoped>
-.out{
+.out {
   width: 100%;
   padding: 0 10px;
-  .title{
+  .title {
     height: 50px;
     line-height: 50px;
-    border-bottom: 1px dashed  #ccc;
+    border-bottom: 1px dashed #ccc;
   }
-  .list{
+  .list {
     display: flex;
     justify-content: space-between;
     padding: 10px;
     border-bottom: 1px solid #ccc;
-    .left{
+    .left {
       display: flex;
       align-items: center;
-      img{
+      img {
         width: 150px;
         height: 100px;
       }
-      .content{
+      .content {
         height: 100px;
         display: flex;
         flex-direction: column;
@@ -171,9 +191,9 @@ export default {
         justify-content: space-between;
       }
     }
-    .right{
+    .right {
       font-size: 12px;
-      span{
+      span {
         padding-left: 10px;
       }
     }
