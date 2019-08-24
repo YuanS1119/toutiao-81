bfaa2621dd87bb7201ac691e59eb592f48cc99e5
YuanS1119 <1650216381@qq.com>
2019-08-24 11:51:41 +0800

    分页组件及请求

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -7,17 +7,31 @@
     <template>
       <el-table style="width: 100%" :data="commentInfo" stripe>
         <el-table-column prop="title" label="标题" width="400"></el-table-column>
-        <el-table-column :formatter="formatter" prop="comment_status" label="评论状态" width="145"></el-table-column>
-        <el-table-column prop="total_comment_count" label="评论总数" width="145"></el-table-column>
-        <el-table-column prop="fans_comment_count" label="粉丝评论数" width="145"></el-table-column>
-        <el-table-column label="操作" width="145">
+        <el-table-column :formatter="formatter" prop="comment_status" label="评论状态"></el-table-column>
+        <el-table-column prop="total_comment_count" label="评论总数"></el-table-column>
+        <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+        <el-table-column label="操作">
           <template slot-scope="obj">
             <el-button type="text">修改</el-button>
-            <el-button @click='closeOrOpen(obj.row)' :style="{color: obj.row.comment_status ? '#E6A23C' : '#409EFF'}" type="text">{{ obj.row.comment_status ? "关闭评论" : "打开评论" }}</el-button>
+            <el-button
+              @click="closeOrOpen(obj.row)"
+              :style="{color: obj.row.comment_status ? '#E6A23C' : '#409EFF'}"
+              type="text"
+            >{{ obj.row.comment_status ? "关闭评论" : "打开评论" }}</el-button>
           </template>
         </el-table-column>
       </el-table>
     </template>
+    <el-row type="flex" justify="space-around">
+      <el-pagination
+        background
+        :total="page.totalPage"
+        :page-size="page.pageSize"
+        :current-page="page.currentPage"
+        @current-change="changePage"
+        layout="prev, pager, next"
+      ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -25,20 +39,35 @@
 export default {
   data () {
     return {
-      commentInfo: []
+      commentInfo: [],
+      page: {
+        pageSize: 10,
+        totalPage: 0,
+        currentPage: 1
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getComments()// 每次改变需要重新查询一次
+    },
     // 查询评论数据
     getComments () {
+      let page = this.page.currentPage// 页数
+      // let per_page = this.page.pageSize// 每页数量
+      let transformPage = { page, per_page: this.page.pageSize }
+      console.log(transformPage)
       this.$axios({
         url: '/articles',
         params: {
-          response_type: 'comment'
+          response_type: 'comment',
+          ...transformPage
         }
       }).then(res => {
         console.log(res)
         this.commentInfo = res.data.results // 将取到的数据给当前数据对象
+        this.page.totalPage = res.data.total_count
       })
     },
     // table中写好的过滤属性
