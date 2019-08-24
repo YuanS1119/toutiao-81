335ec50caf18ccd7ca251d133e6e8939d1fda178
YuanS1119 <1650216381@qq.com>
2019-08-24 10:51:20 +0800

    黑马头条PC-评论列表-打开和关闭评论ANG统一处理大数字类型

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,6 +1,34 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
+import JSONBig from 'json-bigint'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+
+/*
+  后端=>前端=>字符串=>JSON.parse()=>如果超出了最大安全数字=>计算偏差=>JSON.parse()将不精确
+  js的数字有最大安全数值
+    即2的53次方
+    Number.MAX_SAFE_INTEGER
+    在运算或者json对象转换时会有误差
+  解决方法：
+    json-bigint npm包
+      1.看文档
+      2.npm i json-bigint
+      3.import JSONBig from 'json-bigint'
+      4.JSONBig.parse('数据')
+    使用：
+      原来是axios默认转换数据
+      现在使用自定义的方式转换数据
+      transformResponse 在传递给then/catch前，允许修改响应数据
+      eg：tansfromResponse:[function(data){
+        对data进行任意转换处理
+        return data:
+      }],
+  结论：后端传过来的id数字超过了前端最大安全数字的限制，导致JSON.parse及其他运算失败，需要第三方的转化包
+  jso-bigint=>调用接口=>ok
+*/
+axios.defaults.transformResponse = [function (data) {
+  return data ? JSONBig.parse(data) : {}
+}]
 // 请求拦截器
 axios.interceptors.request.use(function (config) {
   // 在发起请求请做一些业务处理
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -6,13 +6,15 @@
     <!-- 表格组件 -->
     <template>
       <el-table style="width: 100%" :data="commentInfo" stripe>
-        <el-table-column prop='title' label="标题" width="400"></el-table-column>
-        <el-table-column :formatter='formatter' prop='comment_status' label="评论状态" width="145"></el-table-column>
-        <el-table-column prop='total_comment_count' label="评论总数" width="145"></el-table-column>
-        <el-table-column prop='fans_comment_count' label="粉丝评论数" width="145"></el-table-column>
-        <el-table-column label="操作" width="145" >
-          <el-button type="text">修改</el-button>
-          <el-button type="text">关闭评论</el-button>
+        <el-table-column prop="title" label="标题" width="400"></el-table-column>
+        <el-table-column :formatter="formatter" prop="comment_status" label="评论状态" width="145"></el-table-column>
+        <el-table-column prop="total_comment_count" label="评论总数" width="145"></el-table-column>
+        <el-table-column prop="fans_comment_count" label="粉丝评论数" width="145"></el-table-column>
+        <el-table-column label="操作" width="145">
+          <template slot-scope="obj">
+            <el-button type="text">修改</el-button>
+            <el-button @click='closeOrOpen(obj.row)' :style="{color: obj.row.comment_status ? '#E6A23C' : '#409EFF'}" type="text">{{ obj.row.comment_status ? "关闭评论" : "打开评论" }}</el-button>
+          </template>
         </el-table-column>
       </el-table>
     </template>
@@ -36,13 +38,27 @@ export default {
         }
       }).then(res => {
         console.log(res)
-        this.commentInfo = res.data.results// 将取到的数据给当前数据对象
+        this.commentInfo = res.data.results // 将取到的数据给当前数据对象
       })
     },
+    // table中写好的过滤属性
     formatter (row, column, cellValue, index) {
       return cellValue ? '正常' : '关闭'
+    },
+    closeOrOpen (row) {
+      // console.log(row.id.toString())
+      let status = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`确认${status}评论吗？`, '提示').then(() => {
+        this.$axios({
+          method: 'put',
+          url: '/comments/status',
+          params: { article_id: row.id.toString() },
+          data: { allow_comment: !row.comment_status }
+        }).then(() => {
+          this.getComments()
+        })
+      })
     }
-
   },
   created () {
     this.getComments()
